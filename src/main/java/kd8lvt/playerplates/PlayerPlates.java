e5a58f3734e40afdd21f860ef079c684be0c1c8a
PureSpider <alex.lamprecht1@gmail.com>
2023-03-26 20:35:57 +0200

    use annotations for plugin.yml generation

--- src/main/java/kd8lvt/playerplates/PlayerPlates.java
+++ src/main/java/kd8lvt/playerplates/PlayerPlates.java
@@ -2,7 +2,15 @@ package kd8lvt.playerplates;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+import org.bukkit.plugin.java.annotation.plugin.author.Authors;
 
+@Plugin(name = "PlayerPlates", version = "1.1")
+@ApiVersion(ApiVersion.Target.v1_13)
+@Author("kd8lvt")
+@Author("PureSpider")
 public final class PlayerPlates extends JavaPlugin {
 
     @Override
